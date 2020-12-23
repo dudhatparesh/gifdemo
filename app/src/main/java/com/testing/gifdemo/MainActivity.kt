8290bb5af5c2c20dbf6892490fc915d576e12e5b
Paresh Dudhat <dudhatparesh@gmail.com>
2020-12-23 11:02:36 +0530

    Added initial stop with loop count

--- app/src/main/java/com/testing/gifdemo/MainActivity.kt
+++ app/src/main/java/com/testing/gifdemo/MainActivity.kt
@@ -1,17 +1,58 @@
 package com.testing.gifdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val imageView = findViewById<ImageView>(R.id.imageView)
-        Glide.with(this).asGif().load("https://i.imgur.com/1ALnB2s.gif").into(imageView)
+
+        Glide.with(this).asGif().load("https://i.imgur.com/1ALnB2s.gif")
+            .listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: GifDrawable?,
+                    model: Any?,
+                    target: Target<GifDrawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    resource?.setLoopCount(1)
+                    resource?.registerAnimationCallback(object :
+                        Animatable2Compat.AnimationCallback() {
+                        override fun onAnimationEnd(drawable: Drawable) {
+                            if(drawable is GifDrawable){
+                                drawable.stop()
+                            }
+                        }
+                    })
+                    return false
+                }
+
+            })
+            .into(imageView)
+
         imageView.setOnClickListener {
             if (imageView.drawable is GifDrawable) {
                 val drawable = imageView.drawable as GifDrawable
